a7254ea6180144cf60b8ab20ab8b5baf0b3c9b26
Samuel Tosin <Samuellyworld@gmail.com>
2023-05-17 23:13:40 +0100

    Revert "Solana pay int"

--- server/routes/payment.router.ts
+++ server/routes/payment.router.ts
@@ -1,17 +1,11 @@
 // importing relevant modules
 import express from 'express';
-import { httpConvertUSDC, initializePayment, validatePayment } from '../controllers/payment.controller';
+import { httpConvertUSDC } from '../controllers/payment.controller';
 
 // creating 'mock-up' payment route
 export const paymentRouter = express.Router();
 
 // getting all USDT conversion rate
 paymentRouter.get('/', httpConvertUSDC);
-
-// Initialize new payment.
-paymentRouter.post('/initialize', initializePayment);
-
-// Validate a payment.
-paymentRouter.post('/validate', validatePayment);
 
 
--- server/types/config.types.ts
+++ server/types/config.types.ts
@@ -1,9 +1,7 @@
 // defaultConfig Types
 export interface defaultConfigTypes {
     PORT: number,
-    FLW_PUBLIC_KEY: string,
-    FLW_SECRET_KEY: string,
-    MERCHANT_ADDRESS: string,
-    DATABASE_URL: string
+    FLW_PUBLIC_KEY:string,
+    FLW_SECRET_KEY:string,
  }
  
